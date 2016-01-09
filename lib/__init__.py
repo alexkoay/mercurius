2f743df9380c085148494b56867a37b333b32c59
Alex Koay <alexkoay88@gmail.com>
2016-01-10 00:59:55 +0800

    Transfer logging level declaration to module init file. Removed doubled declaration.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,4 @@
-import sys
 import logging
-
 logging.addLevelName(10, 'debug')
 logging.addLevelName(12, 'sql')
 logging.addLevelName(15, 'verbose')
@@ -8,4 +6,3 @@ logging.addLevelName(20, 'info')
 logging.addLevelName(30, 'warning')
 logging.addLevelName(40, 'error')
 logging.addLevelName(50, 'critical')
-logging.basicConfig(level=13, format='{asctime} [{name}] {message}', datefmt='%Y-%m-%d %H:%M:%S', style='{')
